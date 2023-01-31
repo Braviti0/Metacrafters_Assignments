3fba6fd7e6b6e030ecf0c8d3e3a343ca38539e5b
Braviti0 <84294327+Braviti0@users.noreply.github.com>
2023-01-31 01:01:14 +0000

    completed

--- Solidity_Intermediate/module3/Challenge2.sol
+++ Solidity_Intermediate/module3/Challenge2.sol
@@ -1,32 +1,54 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
 
-interface ICounter {
-    function count() external view returns (uint);
+pragma solidity >=0.7.0 <0.9.0; //solidity version
 
-    function increment() external;
+// sample interface
+interface falseToken{
+    function mint (uint _amount) external;
+    function balanceOf () external view returns (uint);
+    function burn (uint _amount) external;
 }
 
-contract MyContract {
-    function incrementCounter(address _counter) external {
-        ICounter(_counter).increment();
+// sample abstract (has undefined functions inherited from interface)
+// requires abstract keyword
+abstract contract trueToken is falseToken {
+    // function simTransfer (allows one to check balance left after transfer)
+    function simTransfer (uint balance, uint amount) public pure returns (uint){
+        return (balance - amount);
+    }
+}
+
+// Contract for a primitive token that is mintable and burnable but cannot be transferred
+contract Mytoken is trueToken{
+
+    // Declaration of state variables (owner of tokens and contract) and totalSupply of tokens
+    uint totalSupply;
+    address owner;
+
+    // Constructor sets the owner of the contract during deployment
+    constructor(){
+        owner = msg.sender;
     }
 
-    function getCount(address _counter) external view returns (uint) {
-        return ICounter(_counter).count();
+    // modifier (makes sure only the owner can access a given function)
+    modifier onlyOwner { 
+        require (owner == msg.sender, "only owner can access");
+        _;
     }
-}
+    // All functions below can only be accessed by the owner
 
-pragma solidity ^0.5.0;
+    // mint function (allows owner to add tokens)
+    function mint (uint _amount) public override onlyOwner{
+        totalSupply = totalSupply + _amount;
+    }
 
-contract CalciAbstract {
-   function getResult() public view returns(uint);
-}
-contract Compute is CalciAbstract {
-   function getResult() public view returns(uint) {
-      uint a = 1;
-      uint b = 2;
-      uint result = a + b;
-      return result;
-   }
+    // function balanceOf (allows owner to check the balance)
+    function balanceOf() public view override onlyOwner returns(uint){
+        return totalSupply;
+    }
+
+    // function burn (allows owner to remove tokens)
+    function burn (uint _amount) public override onlyOwner {
+        totalSupply = totalSupply - _amount;
+    }
 }
